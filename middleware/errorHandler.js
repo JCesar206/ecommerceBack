5a406ed5381c5b344b5e20810d0eb1619128a5ab
Julio Cesar <jcesar206@hotmail.com>
2026-07-10 09:48:51 -0600

    Se agrega archivo errorHandler.js para el manejo de errores de la app

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,12 +1,14 @@
 import logger from "../utils/logger.js";
+import { HTTP_STATUS } from "../constants/httpStatus.js";
 const errorHandler = (err,req,res,next)=>{
 	logger.error({
-		message:err.message,
-		stack:err.stack,
-		url:req.originUrl,
-		method:req.method
+		message: err.message,
+		status: err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR,
+		method: req.method,
+		url: req.originUrl,
+		stack: err.stack
 	});
-	res.status(err.status || 500).json({success:false, message:err.message || "Error interno del servidor"});
+	res.status(err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR).json({success:false, message:err.message || "Error interno del servidor"});
 };
 
 export default errorHandler;
