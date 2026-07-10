63b45346a593af21c25f9440640e387845a29961
Julio Cesar <jcesar206@hotmail.com>
2026-07-10 09:05:16 -0600

    Se agrega archivo messages.js para mostrar mensajes centralizados en consola

---
+++ constants/messages.js
@@ -0,0 +1,16 @@
+export const MESSAGES = Object.freeze({
+	REGISTER_SUCCESS: "Usuario registrado correctamente",
+	LOGIN_SUCCESS: "Inicio de sesión exitoso",
+	LOGOUT_SUCCESS: "Sesión cerrada correctamente",
+	REFRESH_SUCCESS: "Token actualizado correctamente",
+	INVALID_CREDENTIALS: "Correo o contraseña incorrectos",
+	USER_EXISTS: "El correo ya está registrado",
+	USER_NOT_FOUND: "Usuario no encontrado",
+	TOKEN_REQUIRED: "Token requerido",
+	INVALID_TOKEN: "Token inválido",
+	ACCESS_DENIED: "Acceso denegado",
+	PRODUCT_CREATED: "Producto creado correctamente",
+	PRODUCT_UPDATED: "Producto actualizado correctamente",
+	PRODUCT_DELETE: "Producto eliminado correctamente",
+	INTERNAL_ERROR: "Errir interno del servidor"
+});
