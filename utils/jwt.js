11655c06764b4d8175b2eb5c6d292ce49eda1d4e
Julio Cesar <jcesar206@hotmail.com>
2026-07-10 09:08:58 -0600

    Se agrega archivo para manejo de tokens y errores en tokens de acceso dentro la app

---
+++ utils/jwt.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+export const generateAccessToken = (user) => {
+	return jwt.sign(
+		{
+			id: user.id,
+			role: user.role
+	},
+	process.env.JWT_ACCESS_SECRET,
+	{expiresIn: process.env.ACCESS_TOKEN_EXPIRE}
+	);
+};
+export const generateRefreshToken = (user) => {
+	return jwt.sign(
+		{
+			id: user.id
+		},
+		process.env.JWT_REFRESH_TOKEN,
+		{expiresIn: process.env.REFRESH_TOKEN_EXPIRE}
+	);
+};
+export const verifyAccessToken = (token) => {
+	return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+};
+export const verifyRefreshToken = (token) => {
+	return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+};
