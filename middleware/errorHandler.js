2dc52be9803619ab6ee0c10c5e76e9cb6d64c4f9
Julio Cesar <jcesar206@hotmail.com>
2026-07-11 09:13:16 -0600

    Se actualiza lógica del archivo errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,11 +1,12 @@
 import logger from "../utils/logger.js";
 import { HTTP_STATUS } from "../constants/httpStatus.js";
+
 const errorHandler = (err,req,res,next)=>{
 	logger.error({
 		message: err.message,
 		status: err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR,
 		method: req.method,
-		url: req.originUrl,
+		url: req.originalUrl,
 		stack: err.stack
 	});
 	res.status(err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR).json({success:false, message:err.message || "Error interno del servidor"});
