e29107e292aa2e7e52669a663527cf3a3dd493d6
Julio Cesar <jcesar206@hotmail.com>
2026-07-11 06:28:05 -0600

    Se agrega roleMiddleware.js para el manejo de roles de usuario de la app

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,14 @@
+import AppError from "../utils/AppError.js";
+import { HTTP_STATUS } from "../constants/httpStatus.js";
+import { MESSAGES } from "../constants/messages.js";
+
+const roleMiddleware = (...roles) => {
+	return (req,res,next) => {
+		if (!roles.includes(req.user.role)) {
+			return next(new AppError(MESSAGES.ACCESS_DENIED, HTTP_STATUS.FORBIDDEN));
+		}
+		next();
+	}
+};
+
+export default roleMiddleware;
