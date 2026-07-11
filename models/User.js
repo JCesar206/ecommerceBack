9e00abbd8b0130a092b29f7b4e6d0d06c5b09cef
Julio Cesar <jcesar206@hotmail.com>
2026-07-11 09:14:50 -0600

    Se actualiza lógica de archivo User.js para el buen manejo de datos de usuario

--- models/User.js
+++ models/User.js
@@ -13,6 +13,13 @@ const User = {
 		const [result] = await pool.query(`INSERT INTO users(name,email,password,role) VALUES (?,?,?,?)`,
 			[name,email,password,role]);
 			return result.insertId;
+	},
+	async updateRole(id, role) {
+		await pool.query(`UPDATE users SET role=? WHERE id=?`, [role, id]);
+	},
+	async exists(id) {
+		const [rows] = await pool.query(`SELECT id FROM users WHERE id=?`, [id]);
+		return rows.length > 0;
 	}
 };
 
