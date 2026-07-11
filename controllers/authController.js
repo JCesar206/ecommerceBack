0a6f9f4cfc9b8625fac927af6f02dfa15126d37d
Julio Cesar <jcesar206@hotmail.com>
2026-07-11 09:11:05 -0600

    Se actualiza lógica del controlador auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,16 @@
-import authService from "../services/authServices.js";
+import authService from "../services/authService.js";
+import USer from "../models/User.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { HTTP_STATUS } from "../constants/httpStatus.js";
 import { MESSAGES } from "../constants/messages.js";
+import { verifyRefreshToken } from "../utils/jwt.js";
 
 const register = asyncHandler(async(req,res) => {
 	const user = await authService.register(req.body);
 	return ApiResponse.success(res, HTTP_STATUS.CREATED, MESSAGES.REGISTER_SUCCESS, user);
 });
+
 const login = asyncHandler(async(req,res) => {
 	const result = await authService.login(req.body);
 	res.cookie("refreshToken", result.refreshToken, {
@@ -21,9 +24,24 @@ const login = asyncHandler(async(req,res) => {
 		accessToken: result.accessToken
 	});
 });
+
 const me = asyncHandler(async (req,res) => {
 	const user = await User.findById(req.user.id);
 	return ApiResponse.success(res, HTTP_STATUS.OK, "Usuario autenticado", user);
 });
 
-export default {register, login, me};
+const refresh = asyncHandler(async (req,res) => {
+	const token = req.cookies.refreshToken;
+	const result = await authService.refresh(token);
+	return ApiResponse.success(res, HTTP_STATUS.OK, MESSAGES.REFRESH_SUCCESS, result);
+});
+
+const logout = asyncHandler(async (req,res) => {
+	const token = req.cookies.refreshToken;
+	const payload = verifyRefreshToken(token);
+	await authService.logout(payload.id);
+	res.clearCookie("refreshToken");
+	return ApiResponse.success(res, HTTP_STATUS.OK, MESSAGES.LOGOUT_SUCCESS);
+});
+
+export default {register, login, refresh, logout, me};
