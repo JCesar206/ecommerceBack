69e9c4ab3b1faea3c6d9fe9d2eb5790cd8a5a097
Julio Cesar <jcesar206@hotmail.com>
2026-07-11 09:14:09 -0600

    Se actualiza lógica del archivo RefreshToken para manejo adecuado de tokens

--- models/RefreshToken.js
+++ models/RefreshToken.js
@@ -1,18 +1,15 @@
 import pool from "../config/db.js";
 const RefreshToken = {
-	async create(userId, token, expiresAt) {
-		await pool.query(`INSERT INTO refresh_tokens(user_id,token,expires_at) VALUES (?,?,?)`,
-			[userId,token,expiresAt]);
+	async create(userId, tokenHash, expiresAt) {
+		await pool.query(`INSERT INTO refresh_tokens(user_id, token, expires_at) VALUES (?,?,?)`,
+			[userId,tokenHash,expiresAt]);
 	},
-	async find(token) {
-		const [rows] = await pool.query(`SELECT * FROM refresh_tokens WHERE token=?`, [token]);
+	async findByUserId(userId) {
+		const [rows] = await pool.query(`SELECT * FROM refresh_tokens WHERE user_id=? LIMIT 1`, [userId]);
 		return rows[0];
 	},
-	async delete(token) {
-		await pool.query(`DELETE FROM refresh_tokens WHERE token=?`, [token]);
-	},
 	async deleteByUser(userId) {
-		await pool.query(`DELETE FROM refresh_tokens WHERE id=?`, [userId]);
+		await pool.query(`DELETE FROM refresh_tokens WHERE user_id=?`, [userId]);
 	}
 };
 
