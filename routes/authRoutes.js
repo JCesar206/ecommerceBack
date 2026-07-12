51ac9f82faa9ec404881040b7a8a0d1880d22ff2
Julio Cesar <jcesar206@hotmail.com>
2026-07-12 08:34:54 -0600

    Se actualiza la lógica del archivo para protección de rutas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,11 @@ import authController from "../controllers/authController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = Router();
+
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.post("/refresh", authController.refresh);
+router.post("/logout", authController.logout);
 router.get("/me", authMiddleware, authController.me);
 
 export default router;
