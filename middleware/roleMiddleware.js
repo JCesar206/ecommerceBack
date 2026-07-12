60bda2c9f670f53ff56455b17062582473c0f201
Julio Cesar <jcesar206@hotmail.com>
2026-07-12 08:34:15 -0600

    Se actualiza la lógica del roleMiddleware para manejo de rol user y admin

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -4,6 +4,10 @@ import { MESSAGES } from "../constants/messages.js";
 
 const roleMiddleware = (...roles) => {
 	return (req,res,next) => {
+		if (!req.user) {
+			return next(new AppError(MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED));
+		}
+
 		if (!roles.includes(req.user.role)) {
 			return next(new AppError(MESSAGES.ACCESS_DENIED, HTTP_STATUS.FORBIDDEN));
 		}
