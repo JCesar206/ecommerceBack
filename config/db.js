667edc0fdc1da90596b9de4d7ef07a9c578f332d
Julio Cesar <jcesar206@hotmail.com>
2026-07-22 11:53:28 -0600

    Se actualiza conexiones y scripts para conexión con db Aiven SQL

--- config/db.js
+++ config/db.js
@@ -1,28 +1,26 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
 import logger from "../utils/logger.js";
-dotenv.config();
+
 const pool = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 	port: process.env.DB_PORT,
-	waitForConnections:true,
-	connectionLimit:10,
-	queueLimit:0
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
 });
 
-const testConnection = async()=>{
-	try{
+const testConnection = async () => {
+	try {
 		const connection = await pool.getConnection();
 		logger.info("MySQL conectado correctamente");
 		connection.release();
 	} catch (error) {
-		logger.error(`Error conectando MySQL ${error.message}`);
-		process.exit(1);
+		logger.error(`Error conectando MySQL: ${error.message}`);
 	}
 };
-
 testConnection();
+
 export default pool;
--- database/init.js
+++ database/init.js
@@ -18,7 +18,7 @@ const createDatabase = async()=>{
 			name VARCHAR(100) NOT NULL,
 			email VARCHAR(150) UNIQUE NOT NULL,
 			password VARCHAR(255) NOT NULL,
-			role ENUM("admin","user") DEFAULT "user",
+			role ENUM('admin','user') DEFAULT 'user',
 			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`);
 
 		await connection.query(`CREATE TABLE IF NOT EXISTS products(
@@ -46,7 +46,7 @@ const createDatabase = async()=>{
 			image VARCHAR(255) NOT NULL,
 			price DECIMAL(10,2) NOT NULL,
 			stock INT NOT NULL DEFAULT 0,
-			created_at TIMESTAMP DEFAULT CURRENT_DEFAULT,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 			updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);`);
 			
 			console.log("Base de datos creada correctamente");
