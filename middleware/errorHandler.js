cbb1304815e547b7a5eabada9debb348fb49832b
Julio Cesar <jcesar206@hotmail.com>
2026-07-23 08:20:29 -0600

    Se actualiza estructura del backend y logica de errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,20 +1,3 @@
-/* import logger from "../utils/logger.js";
-import { HTTP_STATUS } from "../constants/httpStatus.js";
-
-const errorHandler = (err,req,res,next)=>{
-	logger.error({
-		message: err.message,
-		status: err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR,
-		method: req.method,
-		url: req.originalUrl,
-		stack: err.stack
-	});
-	res.status(err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR).json({success:false, message:err.message || "Error interno del servidor"});
-};
-
-export default errorHandler;
-*/
-
 import logger from "../utils/logger.js";
 import { HTTP_STATUS } from "../constants/httpStatus.js";
 
